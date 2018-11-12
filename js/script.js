f22d08c1ea4b51b358e7864ae10610ef9438351c
sergeykk1987 <sergey@barcelonacodeschool.com>
2018-11-12 21:51:08 +0300

    это наш первый комит

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+a=5;
+
+console.log(a);
+
+let person ={
+
+    name: "john",
+    age: 25,
+    isMarried: false
+
+
+};
+
+//console.log(person['name']);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//console.log(arr[0])
+
+// alert("Hello world");
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = prompt('Есть ли вам 18?', "Да");
+//console.log(typeof(arr));
+
+//console.log("arr" + " - object")
+
+
+let incr = 10, decr = 10;
+
+
+
+
+console.log(incr--);
+console.log(decr++);
+
+console.log("2" === 2);
+
+let isChecked = true, isClose = false;
+
+console.log(isChecked || !isClose);
